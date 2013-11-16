2c2dbe5a277a024dc7a16e9d4b96f4755a83a9e8
Guillermo Indalecio Fernandez <guillermobox@gmail.com>
2013-11-16 12:31:55 +0100

    Used indent -kr -i8 to fix indentation

--- ddcfg.c
+++ ddcfg.c
@@ -4,23 +4,24 @@
 #include "hash.h"
 #include "ini.h"
 
-static char *errorNotFound =
-	"ddcfg:Error when reading %s.%s: not found\n";
+static char *errorNotFound = "ddcfg:Error when reading %s.%s: not found\n";
 static char *errorNotParse =
-	"ddcfg:Error whenc casting %s.%s to type %s: '%s' not parseable\n";
+    "ddcfg:Error whenc casting %s.%s to type %s: '%s' not parseable\n";
 
-static void die(const char *section, const char *option, const char *cast, const char *value)
+static void die(const char *section, const char *option, const char *cast,
+		const char *value)
 {
 	if (cast == NULL || value == NULL) {
 		fprintf(stderr, errorNotFound, section, option);
 	} else {
-		fprintf(stderr, errorNotParse, section, option, cast, value);
+		fprintf(stderr, errorNotParse, section, option, cast,
+			value);
 	}
 	exit(EXIT_FAILURE);
 }
 
-static int handler(void* configuration, const char *section,
-		const char *option, const char *value)
+static int handler(void *configuration, const char *section,
+		   const char *option, const char *value)
 {
 	char *newstr;
 
@@ -40,10 +41,10 @@ int ddcfg_parse_args(int argc, char *argv[])
 	char *key, *value;
 	int i;
 
-	for(i=0; i<argc; i++){
+	for (i = 0; i < argc; i++) {
 		if (strcmp(argv[i], "--config") == 0) {
-			if (i+2 >= argc)
-				return -1; /* return error */
+			if (i + 2 >= argc)
+				return -1;	/* return error */
 			key = argv[++i];
 			value = argv[++i];
 			install(key, value);
@@ -86,7 +87,7 @@ int ddcfg_int(const char *section, const char *option)
 
 	answer = ddcfg_get(section, option);
 	ptr = answer;
-	number = (int)strtol(answer, &ptr, 10);
+	number = (int) strtol(answer, &ptr, 10);
 	if (ptr == answer)
 		die(section, option, "int", answer);
 	return number;
@@ -103,9 +104,9 @@ int ddcfg_bool(const char *section, const char *option)
 		if (lower[i] >= 'A' && lower[i] <= 'Z')
 			lower[i] += 32;
 	}
-	if (strcmp(lower, "true") == 0 || strcmp(lower,"1") == 0)
+	if (strcmp(lower, "true") == 0 || strcmp(lower, "1") == 0)
 		return 1;
-	else if (strcmp(lower, "false") == 0 || strcmp(lower,"0") == 0)
+	else if (strcmp(lower, "false") == 0 || strcmp(lower, "0") == 0)
 		return 0;
 	else
 		die(section, option, "bool", answer);
--- ddcfg.h
+++ ddcfg.h
@@ -6,23 +6,22 @@
 /* Parse a file to the dictionary of key/value pairs. Call this
  * before any other call, otherwise they will return error.
  */
-int	ddcfg_parse(char * filename);
+int ddcfg_parse(char *filename);
 
 /* Parse the arguments from the command line, call it after cfg_parse to
  * mantain the priority of the command line arguments over the configuration
  * file
  */
-int	ddcfg_parse_args(int argc, char **argv);
+int ddcfg_parse_args(int argc, char **argv);
 
 /* This function will return a pointer to a string of the specified
  * section and options. No parsing is done.
  */
-char*	ddcfg_get(const char *section, const char *option);
+char *ddcfg_get(const char *section, const char *option);
 
 /* This functions will return a parsed value for the specified section
  * and options.
  */
-int	ddcfg_int(const char *section, const char *option);
-int	ddcfg_bool(const char * section, const char * option);
-double	ddcfg_double(const char * section, const char * option);
-
+int ddcfg_int(const char *section, const char *option);
+int ddcfg_bool(const char *section, const char *option);
+double ddcfg_double(const char *section, const char *option);
