d26e118a9553c2d9bbfae7e3826eb059816d261b
Guillermo Indalecio <guillermobox@gmail.com>
2013-11-18 17:15:12 +0100

    Added protectors in header file for hash

--- hash.h
+++ hash.h
@@ -1,3 +1,6 @@
+#ifndef __HASH_H__
+#define __HASH_H__
+
 struct nlist {
 	struct nlist *next;
 	char *key;
@@ -6,3 +9,5 @@ struct nlist {
 struct nlist *lookup(const char *);
 struct nlist *install(const char *, const char *);
 char **getall();
+
+#endif
